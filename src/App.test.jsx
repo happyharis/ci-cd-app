7220df5c4330b1ed8fd666eef8b987cedf9b1277
Haris Samingan <muhdharis1@gmail.com>
2024-10-10 12:01:07 +0800

    add fix

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,7 +9,7 @@ test('App form submission', async () => {
   const emailInput = screen.getByTestId('emailInput')
   const submitButton = screen.getByTestId('submitButton')
 
-  await userEvent.type(emailInput, 'haris')
+  await userEvent.type(emailInput, 'haris@sigmaschool.co')
 
   expect(submitButton.disabled).toBe(false)
 })
